5bcdbc7ddefe02d6d89cd10c8889ae80edb1ef89
WillSilvah <williamfelipe235@gmail.com>
2026-02-02 09:59:02 -0300

    Correções

--- comandos/CriarSlashAuto/Fursuit.js
+++
@@ -1,8 +0,0 @@
-module.exports = [{
-name: "readyEvent",
-type: "ready",
-channel: "1465218335389257802",
-code: `
-$createApplicationCommand[global;fursuit;Veja uma imagem aleatória de uma fursuit;false;guild,user;guild,dm;slash]
-`
-}]
--- comandos/CriarSlashAuto/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-name: "readyEvent",
-type: "ready",
-channel: "1465218335389257802",
-code: `
-$createApplicationCommand[global;ping;Pong!;false;guild;guild,botdm,dm;slash]
-
-`
-}]
