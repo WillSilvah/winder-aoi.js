c9405b290a4627d1454d755e0771ce483db0085e
WillSilvah <williamfelipe235@gmail.com>
2026-02-02 21:17:54 -0300

    Correção

--- comandos/Develop/dropEmit.js
+++ comandos/Develop/dropEmit.js
@@ -9,8 +9,8 @@ $setUserVar[pickCount;$sum[$getUserVar[pickCount;$clientID];1];$clientID]
 
 $setGuildVar[pickXPmin;0]
 $setGuildVar[pickXPmax;0]
-$slowmode[0s;1462224055884189781]
-$channelSendMessage[$getVar[batePapo];###  EVENTO DE CHAT FOI CANCELADO!]
+$slowmode[0s;$getGuildVar[batePapo]]
+$channelSendMessage[$getGuildVar[batePapo];###  EVENTO DE CHAT FOI CANCELADO!]
 
 $setGuildVar[pickStatus;false]
 $setGuildVar[pickLastUser;$clientID]
@@ -23,14 +23,14 @@ $wait[1m]
 $setGuildVar[pickXPmin;20]
 $setGuildVar[pickXPmax;30]
 
-$ifAwaited[1==1;{execute:drop-$ifAwaited[$message[1]==;$random[1;4];$message[1]]}
+$ifAwaited[1==1;{execute:drop-$ifAwaited[$message[1]==;$random[1;4];$message[1]]}]
 
 $deleteCommand
 $suppressErrors
-$onlyPerms[administrator;<@$authorID> Você precisa ter a permissão de administrador para usar este comando.]
 
 $useChannel[$getGuildVar[batePapo]]
 
-$setGuildVar[pickEmit;true;861313067291115541]
+$setGuildVar[pickEmit;true;$guildID
+$onlyPerms[administrator;<@$authorID> Você precisa ter a permissão de administrador para usar este comando.]
 `
 }] 
