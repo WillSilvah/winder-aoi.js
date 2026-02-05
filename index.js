4a28fa61e6da6ed0a32c6fa87f9f7102ec123885
WillSilvah <williamfelipe235@gmail.com>
2026-02-05 11:14:29 -0300

    BugFixs

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ client.functionManager.createFunction({
 client.functionManager.createFunction({
     name: "$linuxRam", 
     type: "aoi.js",
-    code: `$exec[ps -o rss= -p $(pgrep -f "winder-bot") | awk '{sum+=$1} END {print int(sum/1024)}']`
+    code: `$exec[ps -o rss= -p $(pgrep -f "winder-bot") | awk '{sum+=$1} END {printf int(sum/1024)}']`
 });
