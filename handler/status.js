bc8f09576bf61f41b96552b860aef04896071e3e
WillSilvah <williamfelipe235@gmail.com>
2026-02-05 22:20:08 -0300

    bugFixs

--- handler/status.js
+++ handler/status.js
@@ -1,9 +1,23 @@
 module.exports = (client) => {
 
 client.status({
-    name: "Example Text!",
+    name: "$linuxRam MB | $cpu% | $uptime[humanize] | $pingms | $getVersion",
     type: "PLAYING",
-    status: "dnd",
+    status: "online",
+    time: 12
+});
+
+client.status({
+    name: "$membersCount[$guildID;all;false] peludos",
+    type: "WATCHING",
+    status: "online",
+    time: 12
+});
+
+client.status({
+    name: "Mais de $getGuildVar[guildMonthMessages;1462224054676099094] neste mês!",
+    type: "LISTENING",
+    status: "online",
     time: 12
 });
 
